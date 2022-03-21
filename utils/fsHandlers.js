0cbe3ccf6a6762cd99eb3862cf907509717c9a87
Ojini Chizoba Jude <judoc96@gmail.com>
2022-03-21 17:16:51 +0100

    promise base fs utils to handle write and read ops

---
+++ utils/fsHandlers.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+/**
+ * a promise based utility to read content of a file
+ * @param {string} filePath path to file
+ * @returns promise
+ */
+function readFileContent(filePath) {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(filePath)) {
+      fs.readFile(filePath, { encoding: "utf8" }, function (error, data) {
+        if (error) {
+          reject(error);
+        }
+        resolve(data);
+      });
+    } else {
+      const error = new Error("Datastore not found, please create one");
+      error.code = 400;
+      reject(error);
+    }
+  });
+}
+
+/**
+ * a promise based utility to write content to file (appends to the end)
+ * @param {string} filePath path to file
+ * @param {string} content content to be written in the file
+ * @returns promise
+ */
+function writeToFile(filePath, content) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, content, { encoding: "utf-8" }, function (error) {
+      if (error) {
+        reject(error);
+      }
+      resolve("Content saved successfully");
+    });
+  });
+}
+
+module.exports = {
+  readFileContent,
+  writeToFile,
+};
